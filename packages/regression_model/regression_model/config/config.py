0fd3a889a8130fa85d14dde27fb0db16a8f7f12b
Rocco Langone <roccolangone@hotmail.com>
2020-03-24 11:01:23 +0100

    fix directory path in config

--- packages/regression_model/regression_model/config/config.py
+++ packages/regression_model/regression_model/config/config.py
@@ -3,7 +3,7 @@ import pathlib
 import regression_model
 
 
-PACKAGE_ROOT = pathlib.Path(regression_model.__file__).resolve().parent
+PACKAGE_ROOT = pathlib.Path(regression_model.__file__).resolve().parent.parent
 TRAINED_MODEL_DIR = PACKAGE_ROOT / 'trained_models'
 DATASET_DIR = PACKAGE_ROOT / 'datasets'
 
